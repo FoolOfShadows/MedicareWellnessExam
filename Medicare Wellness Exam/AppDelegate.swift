2879198834d9eb64b0d4bc4a090c5c9bb2c3a83a
FoolOfShadows <foolofshadows@gmail.com>
2016-08-02 10:10:36 -0500

    Initial Commit

---
+++ Medicare Wellness Exam/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Medicare Wellness Exam
+//
+//  Created by Fool on 8/2/16.
+//  Copyright © 2016 Fulgent Wake. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
